e579faf09d1f9c62f571347f3e200825470f6223
lkyash-PO <ashwin.sharma.mylearning@gmail.com>
2020-04-19 16:46:53 +0530

    First Reactor Commit

---
+++ src/main/java/com/lkyash/reactortest/ReactorTestApplication.java
@@ -0,0 +1,13 @@
+package com.lkyash.reactortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lkyash/reactortest/ReactorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lkyash.reactortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
